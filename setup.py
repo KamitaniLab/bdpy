50dae8d7265a5edfe5a9d1db88ff8bfeff15c359
Shuntaro Aoki <aoki@atr.jp>
2017-08-04 18:49:30 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+'''
+Setup script for bdpy
+
+This file is a part of BdPy.
+'''
+
+
+from setuptools import setup
+
+
+VERSION_FILE = 'version'
+
+
+if __name__ == '__main__':
+
+    # Get version num
+    with open(VERSION_FILE, 'r') as f:
+        version_num = f.readline().strip()
+
+    print 'Version: %s' % version_num
+
+    # Setup
+    setup(name='bdpy',
+          version=version_num,
+          description='Brain decoder toolbox',
+          author='Shuntaro Aoki',
+          author_email='kamitanilab.contact@gmail.com',
+          url='https://github.com/KamitaniLab/bdpy',
+          license='MIT',
+          packages=['bdpy',
+                    'bdpy.bdata',
+                    'bdpy.dataform',
+                    'bdpy.ml',
+                    'bdpy.mri',
+                    'bdpy.preproc',
+                    'bdpy.stats',
+                    'bdpy.util'],
+          install_requires=['numpy', 'scipy', 'scikit-learn'])
