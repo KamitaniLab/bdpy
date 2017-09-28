960745fa7632db7c4a20ae7eaea7a142e5593c2b
Shuntaro Aoki <aoki@atr.jp>
2017-09-28 17:23:29 +0900

    ml/crossvalidation: add cvindex_groupwise()

--- bdpy/ml/__init__.py
+++ bdpy/ml/__init__.py
@@ -6,5 +6,6 @@ This package is a part of BdPy
 
 
 from crossvalidation import make_cvindex,make_crossvalidationindex
+from crossvalidation import cvindex_groupwise
 from ensemble import *
 from regress import *
--- bdpy/ml/crossvalidation.py
+++ bdpy/ml/crossvalidation.py
@@ -1,9 +1,50 @@
-"""
-Utilities for cross-validation
-"""
+'''Cross-validation functions
+
+This module provides utility functions for cross-validation.
+'''
 
 
 import numpy as np
+
+
+def cvindex_groupwise(group, nfolds=None, return_bool=False):
+    '''Return k-fold iterator for group-wise cross-validation (e.g, run-wise, block-wise, ...)
+
+    n_folds` specification are not supported yet.
+
+    Parameters
+    ----------
+    group : array-like (shape =  (n_samples, )
+        Group labels (e.g., run labels, block labels, ...)
+    n_folds : int, optional
+        Number of folds (default: the number of unique elements in `group`)
+    return_bool : bool, optional
+        Return boolean arrays if True (default: False)
+
+    Returns
+    -------
+    K-fold iterator
+    '''
+
+    # TODO: add size checking of group
+
+    group_set = np.unique(group)  # Unique labels in `group`
+
+    if nfolds is None:
+        nfolds = len(group_set)
+
+    for gl in group_set:
+        index_train_bool = (group != gl).flatten()
+        index_test_bool = (group == gl).flatten()
+
+        if return_bool:
+            index_train = index_train_bool
+            index_test = index_test_bool
+        else:
+            index_train = np.where(index_train_bool)[0]
+            index_test = np.where(index_test_bool)[0]
+
+        yield index_train, index_test
 
 
 def make_cvindex(group):
@@ -44,15 +85,15 @@ def make_cvindex(group):
                 [False, False,  True],
                 [False, False,  True]], dtype=bool))
     """
-    
+
     # Get and sort unique group index
     group_set = sorted(list(set(group.flatten())))
-    
-    # The number of sample 
+
+    # The number of sample
     n_sample = len(group);
     # The number of run
     n_group = len(group_set);
-    
+
     if n_group == 0:
         # Training only
         train_index = np.ones((n_sample,))
@@ -66,10 +107,10 @@ def make_cvindex(group):
         group_t = np.reshape(group, (group.shape[0], 1))
         mat_group = np.tile(group_t, (1, n_group))
         mat_group_set = np.tile(np.array(group_set), (n_sample, 1))
-        
+
         train_index = mat_group != mat_group_set
         test_index = mat_group == mat_group_set
-    
+
     return train_index, test_index
 
 
