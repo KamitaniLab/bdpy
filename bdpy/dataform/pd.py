d06c4f5bed92d79a14748b658ee8dd7078d64d03
Shuntaro Aoki <aoki@atr.jp>
2017-10-02 17:08:36 +0900

    dataform/pd: add append_dataframe()

--- bdpy/dataform/pd.py
+++ bdpy/dataform/pd.py
@@ -5,7 +5,7 @@ This file is a part of BdPy
 """
 
 
-__all__ = ['convert_dataframe']
+__all__ = ['convert_dataframe', 'append_dataframe']
 
 
 import pandas as pd
@@ -27,3 +27,20 @@ def convert_dataframe(lst):
     df_lst = (pd.DataFrame([item.values()], columns=item.keys()) for item in lst)
     df = pd.concat(df_lst, axis=0, ignore_index=True)
     return df
+
+
+def append_dataframe(df, **kwargs):
+    '''Append a row to Pandas dataframe `df`
+
+    Parameters
+    ----------
+    df : Pandas dataframe
+    kwargs : key-value of data to be added in `df`
+
+    Returns
+    -------
+    Pandas dataframe
+    '''
+
+    df_append = pd.DataFrame({k : [kwargs[k]] for k in kwargs})
+    return df.append(df_append, ignore_index=True)
