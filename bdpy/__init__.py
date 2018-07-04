9d251fd659420786d1ff9534aa0acbdb76d8d00e
Shuntaro Aoki <aoki@atr.jp>
2018-07-04 12:09:35 +0900

    [fix] fix __init__.py

--- bdpy/__init__.py
+++ bdpy/__init__.py
@@ -1,11 +1,11 @@
 """
-BdPy: Brain decoding package for Python
+BdPy: Brain decoding toolbox for Python
 
-Developed by Kamitani Lab in Kyoto Univ. and ATR
+Developed by Kamitani Lab, Kyoto Univ. and ATR
 """
 
 
 # `import bdpy` implicitly imports class `BData` (in package `bdata`) and
 # package `util`.
-from bdata import BData
-from util import *
+from .bdata import BData
+from .util import *
