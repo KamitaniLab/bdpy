e2bcd53b828e49640cd929c2f6619e824d4fd0e7
Shuntaro Aoki <aoki@atr.jp>
2018-12-06 15:43:17 +0900

    [fix] replace concat_dataset with vstack

--- bdpy/bdata/utils.py
+++ bdpy/bdata/utils.py
@@ -86,7 +86,7 @@ def concat_dataset(data_list, successive=[]):
     successsive : list, optional
         Sucessive columns. The values of columns specified here are inherited
         from the preceding data.
-    
+
     Returns
     -------
     dat : BData
@@ -94,30 +94,8 @@ def concat_dataset(data_list, successive=[]):
 
     Example
     -------
-    
+
         data = concat_dataset([data0, data1, data2], successive=['Session', 'Run', 'Block'])
     '''
 
-    suc_cols = {s : 0 for s in successive}
-
-    dat = BData()
-
-    for ds in data_list:
-        ds_copy = copy.deepcopy(ds)
-        for s in successive:
-            v = ds_copy.select(s)
-            v += suc_cols[s]
-            ds_copy.update(s, v)
-
-        if dat.dataset.shape[0] == 0:
-            dat.dataset = ds_copy.dataset
-            dat.metadata = ds_copy.metadata
-        else:
-            dat.dataset = np.vstack([dat.dataset, ds_copy.dataset])
-            # TODO: add metadat check
-
-        for s in successive:
-            v = dat.select(s)
-            suc_cols[s] = np.max(v)
-
-    return dat
+    return vstack(data_list, successive=successive)
