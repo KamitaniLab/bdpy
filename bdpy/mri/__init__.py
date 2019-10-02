01b6e4bda2a1ce0bb68f5fda2c3637e78626acb3
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2019-10-02 23:39:01 +0900

    [feat] add add_rois and merge_rois

--- bdpy/mri/__init__.py
+++ bdpy/mri/__init__.py
@@ -6,5 +6,5 @@ This package is a part of BdPy
 
 from load_epi import load_epi
 from load_mri import load_mri
-from roi import add_roimask, get_roiflag, add_roilabel
+from roi import add_roimask, get_roiflag, add_roilabel, add_rois, merge_rois
 from fmriprep import create_bdata_fmriprep, FmriprepData
