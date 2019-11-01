f62a4268d5edd1f4a47bd1d4768847a41fbbcdc6
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2019-11-01 12:31:19 +0900

    [chore] update setup.py

--- setup.py
+++ setup.py
@@ -13,16 +13,23 @@ VERSION = 0.13
 
 if __name__ == '__main__':
 
+    # Long description
+    with open('./README.md') as f:
+        long_description = f.read()
+
     # Setup
     setup(name='bdpy',
           version=VERSION,
           description='Brain decoder toolbox for Python',
+          long_description=long_description,
+          long_description_content_type='text/markdown',
           author='Shuntaro C. Aoki',
           author_email='brainliner-admin@atr.jp',
           maintainer='Shuntaro C. Aoki',
           maintainer_email='brainliner-admin@atr.jp',
           url='https://github.com/KamitaniLab/bdpy',
           license='MIT',
+          keywords='neuroscience, neuroimaging, brain decoding, fmri, machine learning',
           packages=['bdpy',
                     'bdpy.bdata',
                     'bdpy.dataform',
