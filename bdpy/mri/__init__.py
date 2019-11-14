cc4805adc8d3230bfa31e1d9bc30dc92ebd9ed0a
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2019-11-14 18:02:04 +0900

    [feat] add create_bdata_spm_domestic

--- bdpy/mri/__init__.py
+++ bdpy/mri/__init__.py
@@ -8,3 +8,4 @@ from load_epi import load_epi
 from load_mri import load_mri
 from roi import add_roimask, get_roiflag, add_roilabel, add_rois, merge_rois
 from fmriprep import create_bdata_fmriprep, FmriprepData
+from spm import create_bdata_spm_domestic
