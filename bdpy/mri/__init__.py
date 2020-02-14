675eb63050590d74a60876001553762c74d807a3
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2020-02-14 18:01:21 +0900

    [feat] add bdpy.mri.export_brain_image

--- bdpy/mri/__init__.py
+++ bdpy/mri/__init__.py
@@ -9,3 +9,4 @@ from load_mri import load_mri
 from roi import add_roimask, get_roiflag, add_roilabel, add_rois, merge_rois
 from fmriprep import create_bdata_fmriprep, FmriprepData
 from spm import create_bdata_spm_domestic
+from image import export_brain_image
