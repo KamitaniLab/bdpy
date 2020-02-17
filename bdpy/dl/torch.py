61255e40afac51a693bdfb265550e92dec21f6fc
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2020-02-17 20:57:40 +0900

    [feat] add bdpy.dl.torch.ImageDataset

---
+++ bdpy/dl/torch.py
@@ -0,0 +1,84 @@
+'''PyTorch module.'''
+
+
+import os
+
+import numpy as np
+from PIL import Image
+import torch
+
+
+class ImageDataset(torch.utils.data.Dataset):
+    '''Pytoch dataset for images.'''
+
+    def __init__(self, images, labels=None, resize=None, shape='chw', transform=None):
+        '''
+        Parameters
+        ----------
+        images : list
+            List of image file paths.
+        labels : list, optional
+            List of image labels (default: image file names).
+        resize : None or tuple, optional
+            If not None, images will be resized by the specified size.
+        shape : str ({'chw', 'hwc', ...}), optional
+            Specify array shape (channel, hieght, and width).
+        transform
+            Transformers (applied after resizing, reshaping, ans scaling to [0, 1])
+
+        Note
+        ----
+        - Images are converted to RGB. Alpha channels in RGBA images are ignored.
+        '''
+
+        image_files = []
+        for imf in images:
+            # TODO: validate the image file
+            image_files.append(os.path.basename(imf))
+
+        self.data_path = images
+        if not labels is None:
+            self.labels = labels
+        else:
+            self.labels = image_files
+        self.n_sample = len(images)
+        self.transform = transform
+        # Custom transforms
+        self.__shape = shape
+        self.__resize = resize
+
+    def __len__(self):
+        return self.n_sample
+
+    def __getitem__(self, idx):
+        img = Image.open(self.data_path[idx])
+
+        # CMYK, RGBA --> RGB
+        if img.mode == 'CMYK':
+            img = img.convert('RGB')
+        if img.mode == 'RGBA':
+            bg = PIL.Image.new('RGB', img.size, (255, 255, 255))
+            bg.paste(img, mask=img.split()[3])
+            img = bg
+
+        data = np.asarray(img)
+
+        # Resize the image
+        if not self.__resize is None:
+            data = np.array(Image.fromarray(data).resize(self.__resize, resample=2))  # bicubic
+
+        # Reshape
+        s2d = {'h': 0, 'w': 1, 'c': 2}
+        data = data.transpose((s2d[self.__shape[0]],
+                               s2d[self.__shape[1]],
+                               s2d[self.__shape[2]]))
+
+        # Scaling to [0, 1]
+        data = data / 255.
+
+        if not self.transform is None:
+            date = self.transform(data)
+        else:
+            data = torch.Tensor(data)
+        label = self.labels[idx]
+        return data, label
