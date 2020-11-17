b0ca0864816a8b3faf52cf8aaf6885a5ba910c1c
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2020-11-17 15:47:24 +0900

    [fix] quick fix for encoding issue on vmap keys

--- bdpy/bdata/utils.py
+++ bdpy/bdata/utils.py
@@ -61,7 +61,7 @@ def vstack(bdata_list, successive=[], metadata_merge='strict', ignore_metadata_d
             # vmap
             vmap_keys = ds_copy.get_vmap_keys()
             for vk in vmap_keys:
-                dat.add_vmap(vk, ds_copy.get_vmap(vk))
+                dat.add_vmap(vk.encode(), ds_copy.get_vmap(vk.encode()))  # FIXME: unicode issue
         else:
             # Check metadata consistency
             if metadata_merge == 'strict':
@@ -98,7 +98,7 @@ def vstack(bdata_list, successive=[], metadata_merge='strict', ignore_metadata_d
             # Merge vmap
             vmap_keys = ds_copy.get_vmap_keys()
             for vk in vmap_keys:
-                dat.add_vmap(vk.encode(), ds_copy.get_vmap(vk))  # FIXME: unicode issue
+                dat.add_vmap(vk.encode(), ds_copy.get_vmap(vk.encode()))  # FIXME: unicode issue
 
         # Update the last values in sucessive columns
         for s in successive:
