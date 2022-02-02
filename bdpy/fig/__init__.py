0c00d29b689839edb84fd84ba9eb65ee2eef2b66
ken.shirakawa <shirakawa.ken.38w@st.kyoto-u.ac.jp>
2022-02-02 15:31:33 +0900

    update movie style

--- bdpy/fig/__init__.py
+++ bdpy/fig/__init__.py
@@ -7,3 +7,4 @@ from .fig import *
 from .tile_images import tile_images
 from .draw_group_image_set import draw_group_image_set
 from .makeplots import makeplots
+from .draw_group_movie_set import draw_group_movie_set, load_video, save_video
