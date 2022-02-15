54b0afe1f91420c3968390a0f6bc1613e9a2b121
micchu <bte0073misato@gmail.com>
2022-02-15 14:23:20 +0900

    [fix] convert all the loaded image to RGB mode

--- bdpy/fig/draw_group_image_set.py
+++ bdpy/fig/draw_group_image_set.py
@@ -78,6 +78,7 @@ def draw_group_image_set(condition_list, background_color = (255, 255, 255),
             if image_filepath is None:
                 continue;
             image_obj = PIL.Image.open(image_filepath)
+            image_obj = image_obj.convert("RGB")
             image_obj = image_obj.resize((image_size[0], image_size[1]), PIL.Image.LANCZOS)
 
             # Calc image position
