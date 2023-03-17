5966ce6a231a8277792e40678b85a28bf6d29812
Yoshihiro Nagano <y.nagano.92@gmail.com>
2023-03-17 04:10:02 +0000

    test cases for dl/torch/models.py

---
+++ test/dl/torch/test_models.py
@@ -0,0 +1,71 @@
+import unittest
+
+import torch
+import torch.nn as nn
+
+from bdpy.dl.torch import models
+
+
+class TestLayerMap(unittest.TestCase):
+    def setUp(self):
+        self.kv_pairs = [
+            {'net': 'vgg19', 'payload': {'key': 'fc6', 'value': 'classifier[0]'}},
+            {'net': 'vgg19', 'payload': {'key': 'conv5_4', 'value': 'features[34]'}},
+            {'net': 'alexnet', 'payload': {'key': 'fc6', 'value': 'classifier[0]'}},
+            {'net': 'alexnet', 'payload': {'key': 'conv5', 'value': 'features[12]'}}
+        ]
+
+    def test_layer_map(self):
+        for kv_pair in self.kv_pairs:
+            expected = kv_pair['payload']
+            output = models.layer_map(kv_pair['net'])
+            self.assertIsInstance(output, dict)
+            self.assertEqual(output[expected['key']], expected['value'])
+
+
+class TestVGG19(unittest.TestCase):
+    def setUp(self):
+        self.input_shape = (1, 3, 224, 224)
+        self.model = models.VGG19()
+
+    def test_forward(self):
+        x = torch.rand(self.input_shape)
+        output = self.model(x)
+        self.assertIsInstance(output, torch.Tensor)
+        self.assertEqual(output.shape, (1, 1000))
+
+    def test_layer_access(self):
+        layer_names = models.layer_map('vgg19').values()
+        for layer_name in layer_names:
+            self.assertIsInstance(
+                models._parse_layer_name(self.model, layer_name), nn.Module)
+
+
+class TestAlexNet(unittest.TestCase):
+    def setUp(self):
+        self.input_shape = (1, 3, 224, 224)
+        self.model = models.AlexNet()
+
+    def test_forward(self):
+        x = torch.rand(self.input_shape)
+        output = self.model(x)
+        self.assertIsInstance(output, torch.Tensor)
+        self.assertEqual(output.shape, (1, 1000))
+
+    def test_layer_access(self):
+        layer_names = models.layer_map('alexnet').values()
+        for layer_name in layer_names:
+            self.assertIsInstance(
+                models._parse_layer_name(self.model, layer_name), nn.Module)
+
+
+class TestAlexNetGenerator(unittest.TestCase):
+    def setUp(self):
+        self.input_shape = (1, 4096)
+        self.model = models.AlexNetGenerator()
+
+    def test_forward(self):
+        x = torch.rand(self.input_shape)
+        output = self.model(x)
+        self.assertIsInstance(output, torch.Tensor)
+        self.assertEqual(output.shape, (1, 3, 256, 256))
