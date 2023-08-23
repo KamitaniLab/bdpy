522d33bc8490b61bd504c631623513e31b625e19
Misato Tanaka <bte0073misato@gmail.com>
2023-08-23 18:09:59 +0900

    [add] add norm1-pool5 generators definition

--- bdpy/dl/torch/models.py
+++ bdpy/dl/torch/models.py
@@ -367,3 +367,447 @@ class AlexNetGenerator(nn.Module):
         g = self.deconv(f)
 
         return g
+
+
+class AlexNetPool5Generator(nn.Module):
+    """ From caffe pool5 generator of bvlc_reference_caffenet.
+    The model trained by DeepSim using ILSVRC dataset.
+    Provided by Alexey Dosovitskiy.
+    """
+
+    def __init__(
+            self, device: Optional[Union[str, Sequence[str]]] = None):
+        super(AlexNetPool5Generator, self).__init__()
+
+        if device is None:
+            self.__device0 = 'cpu'
+            self.__device1 = 'cpu'
+        elif isinstance(device, str):
+            self.__device0 = device
+            self.__device1 = device
+        else:
+            self.__device0 = device[0]
+            self.__device1 = device[1]
+        # input 256x6x6
+        self.Rconv6 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.Rrelu6 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv7 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.Rrelu7 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv8 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=0)
+        self.Rrelu8 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv5 = nn.ConvTranspose2d(
+            512, 256, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv5 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv5_1 = nn.ConvTranspose2d(
+            256, 512, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv5_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv4 = nn.ConvTranspose2d(
+            512, 256, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv4 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv4_1 = nn.ConvTranspose2d(
+            256, 256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv4_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv3 = nn.ConvTranspose2d(
+            256, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv3 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv3_1 = nn.ConvTranspose2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv3_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv2 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(
+            64, 32, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv0 = nn.ConvTranspose2d(
+            32, 3, kernel_size=4, stride=2, padding=1, bias=True)
+        self.rconv = nn.Sequential(
+            self.Rconv6,
+            self.Rrelu6,
+            self.Rconv7,
+            self.Rrelu7,
+            self.Rconv8,
+            self.Rrelu8,
+        ).to(self.__device0)
+        self.deconv = nn.Sequential(
+            self.deconv5,
+            self.relu_deconv5,
+            self.conv5_1,
+            self.relu_conv5_1,
+            self.deconv4,
+            self.relu_deconv4,
+            self.conv4_1,
+            self.relu_conv4_1,
+            self.deconv3,
+            self.relu_deconv3,
+            self.conv3_1,
+            self.relu_conv3_1,
+            self.deconv2,
+            self.relu_deconv2,
+            self.deconv1,
+            self.relu_deconv1,
+            self.deconv0,
+        ).to(self.__device1)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        f = self.rconv(z)
+        g = self.deconv(f)
+        return g
+
+
+class AlexNetRelu4Generator(nn.Module):
+    """ From caffe relu4 generator of bvlc_reference_caffenet. 
+    The model trained by DeepSim using ILSVRC dataset.
+    Provided by Alexey Dosovitskiy.
+    """
+
+    def __init__(
+            self, device: Optional[Union[str, Sequence[str]]] = None):
+        super(AlexNetRelu4Generator, self).__init__()
+
+        if device is None:
+            self.__device0 = 'cpu'
+            self.__device1 = 'cpu'
+        elif isinstance(device, str):
+            self.__device0 = device
+            self.__device1 = device
+        else:
+            self.__device0 = device[0]
+            self.__device1 = device[1]
+        # input 384x13x13
+        self.Rconv6 = nn.Conv2d(384, 384, kernel_size=3,
+                                stride=1, padding=0)  # 384x11x11
+        self.Rrelu6 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv7 = nn.Conv2d(384, 512, kernel_size=3,
+                                stride=1, padding=0)  # 512x9x9
+        self.Rrelu7 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv8 = nn.Conv2d(512, 512, kernel_size=2,
+                                stride=1, padding=0)  # 512x8x8
+        self.Rrelu8 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv5 = nn.ConvTranspose2d(
+            512, 256, kernel_size=4, stride=2, padding=1, bias=True)  # 256x12x12
+        self.relu_deconv5 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv5_1 = nn.ConvTranspose2d(
+            256, 256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv5_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv4 = nn.ConvTranspose2d(
+            256, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv4 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv4_1 = nn.ConvTranspose2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv4_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv3 = nn.ConvTranspose2d(
+            128, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv3 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv3_1 = nn.ConvTranspose2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv3_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv2 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv2_1 = nn.Conv2d(
+            64, 32, kernel_size=3, stride=1, padding=1, bias=True)  # 32x128x128
+        self.relu_conv2_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(
+            32, 16, kernel_size=4, stride=2, padding=1, bias=True)  # 3x256x256
+        self.relu_deconv1 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv1_1 = nn.Conv2d(
+            16, 3, kernel_size=3, stride=1, padding=1, bias=True)  # 3x256x256
+        self.conv1_1_tanh = nn.Tanh()  # Hyperbolic Tangent (Tanh) function  # 3x256x256
+        self.rconv = nn.Sequential(
+            self.Rconv6,
+            self.Rrelu6,
+            self.Rconv7,
+            self.Rrelu7,
+            self.Rconv8,
+            self.Rrelu8,
+        ).to(self.__device0)
+        self.deconv = nn.Sequential(
+            self.deconv5,
+            self.relu_deconv5,
+            self.conv5_1,
+            self.relu_conv5_1,
+            self.deconv4,
+            self.relu_deconv4,
+            self.conv4_1,
+            self.relu_conv4_1,
+            self.deconv3,
+            self.relu_deconv3,
+            self.conv3_1,
+            self.relu_conv3_1,
+            self.deconv2,
+            self.relu_deconv2,
+            self.conv2_1,
+            self.relu_conv2_1,
+            self.deconv1,
+            self.relu_deconv1,
+            self.conv1_1,
+            self.conv1_1_tanh,
+        ).to(self.__device1)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        f = self.rconv(z)
+        g = self.deconv(f)
+        g = g * 255  # conv1_1_tanh_elt layer's scaling
+        return g
+
+
+class AlexNetRelu3Generator(nn.Module):
+    """ From caffe relu3 generator of bvlc_reference_caffenet. 
+    The model trained by DeepSim using ILSVRC dataset.
+    Provided by Alexey Dosovitskiy.
+    """
+
+    def __init__(
+            self, device: Optional[Union[str, Sequence[str]]] = None):
+        super(AlexNetRelu3Generator, self).__init__()
+
+        if device is None:
+            self.__device0 = 'cpu'
+            self.__device1 = 'cpu'
+        elif isinstance(device, str):
+            self.__device0 = device
+            self.__device1 = device
+        else:
+            self.__device0 = device[0]
+            self.__device1 = device[1]
+        # input 384x13x13
+        self.Rconv6 = nn.Conv2d(384, 384, kernel_size=3,
+                                stride=1, padding=0)  # 384x11x11
+        self.Rrelu6 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv7 = nn.Conv2d(384, 512, kernel_size=3,
+                                stride=1, padding=0)  # 512x9x9
+        self.Rrelu7 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv8 = nn.Conv2d(512, 512, kernel_size=2,
+                                stride=1, padding=0)  # 512x8x8
+        self.Rrelu8 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv5 = nn.ConvTranspose2d(
+            512, 256, kernel_size=4, stride=2, padding=1, bias=True)  # 256x12x12
+        self.relu_deconv5 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv5_1 = nn.ConvTranspose2d(
+            256, 256, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv5_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv4 = nn.ConvTranspose2d(
+            256, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv4 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv4_1 = nn.ConvTranspose2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv4_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv3 = nn.ConvTranspose2d(
+            128, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv3 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv3_1 = nn.ConvTranspose2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv3_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv2 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv2_1 = nn.Conv2d(
+            64, 32, kernel_size=3, stride=1, padding=1, bias=True)  # 32x128x128
+        self.relu_conv2_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(
+            32, 16, kernel_size=4, stride=2, padding=1, bias=True)  # 3x256x256
+        self.relu_deconv1 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv1_1 = nn.Conv2d(
+            16, 3, kernel_size=3, stride=1, padding=1, bias=True)  # 3x256x256
+        self.conv1_1_tanh = nn.Tanh()  # Hyperbolic Tangent (Tanh) function  # 3x256x256
+        self.rconv = nn.Sequential(
+            self.Rconv6,
+            self.Rrelu6,
+            self.Rconv7,
+            self.Rrelu7,
+            self.Rconv8,
+            self.Rrelu8,
+        ).to(self.__device0)
+        self.deconv = nn.Sequential(
+            self.deconv5,
+            self.relu_deconv5,
+            self.conv5_1,
+            self.relu_conv5_1,
+            self.deconv4,
+            self.relu_deconv4,
+            self.conv4_1,
+            self.relu_conv4_1,
+            self.deconv3,
+            self.relu_deconv3,
+            self.conv3_1,
+            self.relu_conv3_1,
+            self.deconv2,
+            self.relu_deconv2,
+            self.conv2_1,
+            self.relu_conv2_1,
+            self.deconv1,
+            self.relu_deconv1,
+            self.conv1_1,
+            self.conv1_1_tanh,
+        ).to(self.__device1)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        f = self.rconv(z)
+        g = self.deconv(f)
+        g = g * 255  # conv1_1_tanh_elt layer's scaling
+        return g
+
+
+class AlexNetNorm2Generator(nn.Module):
+    """ From caffe norm2 generator of bvlc_reference_caffenet. 
+    The model trained by DeepSim using ILSVRC dataset.
+    Provided by Alexey Dosovitskiy.
+    """
+
+    def __init__(
+            self, device: Optional[Union[str, Sequence[str]]] = None):
+        super(AlexNetNorm2Generator, self).__init__()
+
+        if device is None:
+            self.__device0 = 'cpu'
+            self.__device1 = 'cpu'
+        elif isinstance(device, str):
+            self.__device0 = device
+            self.__device1 = device
+        else:
+            self.__device0 = device[0]
+            self.__device1 = device[1]
+        # input 256x13x13
+        self.Rconv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=2)
+        self.Rrelu6 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv7 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.Rrelu7 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv8 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.Rrelu8 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv4 = nn.ConvTranspose2d(
+            128, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv4 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv4_1 = nn.Conv2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv4_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv3 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv3 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv3_1 = nn.Conv2d(
+            64, 64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv3_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            64, 32, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv2 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv2_1 = nn.Conv2d(
+            32, 32, kernel_size=3, stride=1, padding=1, bias=True)  # 32x128x128
+        self.relu_conv2_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(
+            32, 16, kernel_size=4, stride=2, padding=1, bias=True)  # 3x256x256
+        self.conv1_1 = nn.Conv2d(
+            16, 3, kernel_size=3, stride=1, padding=1, bias=True)  # 3x256x256
+        self.conv1_1_tanh = nn.Tanh()  # Hyperbolic Tangent (Tanh) function  # 3x256x256
+        self.rconv = nn.Sequential(
+            self.Rconv6,
+            self.Rrelu6,
+            self.Rconv7,
+            self.Rrelu7,
+            self.Rconv8,
+            self.Rrelu8,
+        ).to(self.__device0)
+        self.deconv = nn.Sequential(
+            self.deconv4,
+            self.relu_deconv4,
+            self.conv4_1,
+            self.relu_conv4_1,
+            self.deconv3,
+            self.relu_deconv3,
+            self.conv3_1,
+            self.relu_conv3_1,
+            self.deconv2,
+            self.relu_deconv2,
+            self.conv2_1,
+            self.relu_conv2_1,
+            self.deconv1,
+            self.conv1_1,
+            self.conv1_1_tanh,
+        ).to(self.__device1)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        f = self.rconv(z)
+        g = self.deconv(f)
+        g = g * 255  # conv1_1_tanh_elt layer's scaling
+        return g
+    
+    
+class AlexNetNorm1Generator(nn.Module):
+    """ From caffe norm2 generator of bvlc_reference_caffenet. 
+    The model trained by DeepSim using ILSVRC dataset.
+    Provided by Alexey Dosovitskiy.
+    """
+
+    def __init__(
+            self, device: Optional[Union[str, Sequence[str]]] = None):
+        super(AlexNetNorm1Generator, self).__init__()
+
+        if device is None:
+            self.__device0 = 'cpu'
+            self.__device1 = 'cpu'
+        elif isinstance(device, str):
+            self.__device0 = device
+            self.__device1 = device
+        else:
+            self.__device0 = device[0]
+            self.__device1 = device[1]
+        # input 96x27x27
+        self.Rconv6 = nn.Conv2d(96, 128, kernel_size=3, stride=2, padding=2)
+        self.Rrelu6 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv7 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.Rrelu7 = nn.LeakyReLU(0.3, inplace=True)
+        self.Rconv8 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.Rrelu8 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv4 = nn.ConvTranspose2d(
+            128, 128, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv4 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv4_1 = nn.Conv2d(
+            128, 128, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv4_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv3 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv3 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv3_1 = nn.Conv2d(
+            64, 64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu_conv3_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            64, 32, kernel_size=4, stride=2, padding=1, bias=True)
+        self.relu_deconv2 = nn.LeakyReLU(0.3, inplace=True)
+        self.conv2_1 = nn.Conv2d(
+            32, 32, kernel_size=3, stride=1, padding=1, bias=True)  # 32x128x128
+        self.relu_conv2_1 = nn.LeakyReLU(0.3, inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(
+            32, 16, kernel_size=4, stride=2, padding=1, bias=True)  # 3x256x256
+        self.conv1_1 = nn.Conv2d(
+            16, 3, kernel_size=3, stride=1, padding=1, bias=True)  # 3x256x256
+        self.conv1_1_tanh = nn.Tanh()  # Hyperbolic Tangent (Tanh) function  # 3x256x256
+        self.rconv = nn.Sequential(
+            self.Rconv6,
+            self.Rrelu6,
+            self.Rconv7,
+            self.Rrelu7,
+            self.Rconv8,
+            self.Rrelu8,
+        ).to(self.__device0)
+        self.deconv = nn.Sequential(
+            self.deconv4,
+            self.relu_deconv4,
+            self.conv4_1,
+            self.relu_conv4_1,
+            self.deconv3,
+            self.relu_deconv3,
+            self.conv3_1,
+            self.relu_conv3_1,
+            self.deconv2,
+            self.relu_deconv2,
+            self.conv2_1,
+            self.relu_conv2_1,
+            self.deconv1,
+            self.conv1_1,
+            self.conv1_1_tanh,
+        ).to(self.__device1)
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        f = self.rconv(z)
+        g = self.deconv(f)
+        g = g * 255  # conv1_1_tanh_elt layer's scaling
+        return g
