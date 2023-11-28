7af0d412d7c7d8eb5f6bf94147344651f01c5d60
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2023-11-28 18:15:42 +0900

    [new] add dataset.utils.download_splitted_file

--- bdpy/dataset/utils.py
+++ bdpy/dataset/utils.py
@@ -1,11 +1,20 @@
 """Dataset utilities."""
 
-from typing import Union
+from typing import List, TypedDict, Union
 
 import hashlib
+import inspect
+import os
+import subprocess
 import urllib.request
 
 from tqdm import tqdm
+
+
+class FileDict(TypedDict):
+    name: str
+    url: str
+    md5sum: str
 
 
 def download_file(url: str, destination: str, progress_bar: bool = True, md5sum: Union[str, None] = None) -> None:
@@ -37,6 +46,48 @@ def download_file(url: str, destination: str, progress_bar: bool = True, md5sum:
     with tqdm(total=file_size, unit='B', unit_scale=True, desc=destination, ncols=100) as progress_bar:
         urllib.request.urlretrieve(url, destination, __show_progress)
 
+    if md5sum is not None:
+        md5_hash = hashlib.md5()
+        with open(destination, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), b''):
+                md5_hash.update(chunk)
+        md5sum_test = md5_hash.hexdigest()
+        if md5sum != md5sum_test:
+            raise ValueError(f'md5sum mismatch. \nExpected: {md5sum}\nActual: {md5sum_test}')
+
+
+def download_splitted_file(file_list: List[FileDict], destination: str, progress_bar: bool = True, md5sum: Union[str, None] = None) -> None:
+    """Download a file.
+
+    Parameters
+    ----------
+    file_list: List[FileDict]
+      List of split files.
+    destination: str
+      Path to save the file.
+    progress_bar: bool = True
+      Show progress bar if True.
+    md5sum: Union[str, None] = None
+      md5sum hash of the file.
+
+    Returns
+    -------
+    None
+    """
+    wdir = os.path.dirname(destination)
+
+    # Download split files
+    for sf in file_list:
+        _output = os.path.join(wdir, sf['name'])
+        if not os.path.exists(_output):
+            print(f'Downloading {_output} from {sf["url"]}')
+            download_file(sf['url'], _output, progress_bar=progress_bar, md5sum=sf['md5sum'])
+
+    # Merge files
+    subprocess.run(f"cat {destination}-* > {destination}", shell=True)
+    print(f"File created: {bn}")
+
+    # Check md5sum
     if md5sum is not None:
         md5_hash = hashlib.md5()
         with open(destination, 'rb') as f:
