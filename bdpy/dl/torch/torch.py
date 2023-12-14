083369fda016129911e7abce31b096e1c0b99b1b
Yoshihiro Nagano <nagano@i.kyoto-u.ac.jp>
2023-12-14 19:58:15 +0900

    refactor type annotations

--- bdpy/dl/torch/torch.py
+++ bdpy/dl/torch/torch.py
@@ -1,5 +1,7 @@
 '''PyTorch module.'''
 
+from __future__ import annotations
+
 from typing import Iterable, List, Dict, Union, Tuple, Any, Callable, Optional
 
 import os
@@ -56,10 +58,10 @@ class FeatureExtractor(object):
             layer_object = models._parse_layer_name(self._encoder, layer)
             layer_object.register_forward_hook(self._extractor)
 
-    def __call__(self, x: _tensor_t) -> Dict[str, _tensor_t]:
+    def __call__(self, x: _tensor_t) -> Dict[str, np.ndarray] | Dict[str, torch.Tensor]:
         return self.run(x)
 
-    def run(self, x: _tensor_t) -> Dict[str, _tensor_t]:
+    def run(self, x: _tensor_t) -> Dict[str, np.ndarray] | Dict[str, torch.Tensor]:
         '''Extract feature activations from the specified layers.
 
         Parameters
@@ -82,17 +84,17 @@ class FeatureExtractor(object):
 
         self._encoder.forward(xt)
 
-        features: Dict[str, _tensor_t] = {
+        features: Dict[str, torch.Tensor] = {
             layer: self._extractor.outputs[i]
             for i, layer in enumerate(self.__layers)
         }
-        if self.__detach:
-            features = {
-                k: v.cpu().detach().numpy()
-                for k, v in features.items()
-            }
+        if not self.__detach:
+            return features
 
-        return features
+        return {
+            k: v.cpu().detach().numpy()
+            for k, v in features.items()
+        }
 
 
 class FeatureExtractorHandle(object):
