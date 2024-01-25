6cef2f2a1dbe7331bf2c38babead84de41b2d8ec
yu1120 <65484599+myaokai@users.noreply.github.com>
2024-01-25 16:55:38 +0900

    update test_core

--- tests/dl/torch/domain/test_core.py
+++ tests/dl/torch/domain/test_core.py
@@ -19,6 +19,13 @@ class DummyDoubleDomain(core_module.Domain):
     def receive(self, num):
         return num // 2
     
+class DummyUpperCaseDomain(core_module.Domain):
+    def send(self, text):
+        return text.upper()
+    
+    def receive(self, value):
+        return value.lower()
+    
 class TestDomain(unittest.TestCase):
     """Tests for bdpy.dl.torch.domain.core.Domain."""
     def setUp(self):
@@ -79,5 +86,26 @@ class TestComposedDomain(unittest.TestCase):
     def test_receive(self):
         """test receive"""
         self.assertEqual(self.composed_domian.receive(self.internal_space_num), self.original_space_num)
+
+class TestKeyValueDomain(unittest.TestCase):
+    """Tests for bdpy.dl.torch.domain.core.KeyValueDomain."""
+    def setUp(self):
+        self.key_value_domian = core_module.KeyValueDomain({
+            "name": DummyUpperCaseDomain(),
+            "age": DummyDoubleDomain()
+        })
+        self.original_space_data = {"name": "alice", "age": 30}
+        self.internal_space_data = {"name": "ALICE", "age": 60}
+
+    def test_send(self):
+        """test send"""
+        self.assertEqual(self.key_value_domian.send(self.original_space_data), self.internal_space_data)
+
+    def test_receive(self):
+        """test receive"""
+        self.assertEqual(self.key_value_domian.receive(self.internal_space_data), self.original_space_data)
+
+
+
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/dl/torch/domain/test_feature_domain.py
@@ -0,0 +1,5 @@
+"""Tests for bdpy.dl.torch.domain.feature_domain."""
+
+import unittest
+import torch
+from bdpy.dl.torch.domain import feature_domain as feature_domain_module
